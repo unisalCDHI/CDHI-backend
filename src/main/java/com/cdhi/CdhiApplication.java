3e8c34cb5522ef0682c71447309a7f00dab5915f
Davi <davimargelo@hotmail.com>
2020-02-18 15:12:15 -0300

    first commit

---
+++ src/main/java/com/cdhi/CdhiApplication.java
@@ -0,0 +1,13 @@
+package com.cdhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdhiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdhiApplication.class, args);
+	}
+
+}
