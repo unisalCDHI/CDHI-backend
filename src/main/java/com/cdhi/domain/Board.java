68d1aed82568915777ccdb2f5ac33d6ec3020984
Lab4f <lab4f@admin.local>
2020-02-18 21:46:28 -0300

    my Boards param for User

---
+++ src/main/java/com/cdhi/domain/Board.java
@@ -0,0 +1,65 @@
+package com.cdhi.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Board implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+//    @JsonIgnore
+//    @ManyToMany(mappedBy = "boards")
+//    private Set<User> users = new HashSet<>();
+
+    @JsonIgnore
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public Board() {
+    }
+
+    public Board(String name, User owner) {
+        this.name = name;
+        this.owner = owner;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+//    public Set<User> getUsers() {
+//        return users;
+//    }
+//
+//    public void setUsers(Set<User> users) {
+//        this.users = users;
+//    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+}
--- src/main/java/com/cdhi/services/DBService.java
+++ src/main/java/com/cdhi/services/DBService.java
@@ -1,7 +1,10 @@
 package com.cdhi.services;
 
+import com.cdhi.domain.Board;
 import com.cdhi.domain.User;
+import com.cdhi.repositories.BoardRepository;
 import com.cdhi.repositories.UserRepository;
+import com.cdhi.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +16,23 @@ public class DBService {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    BoardRepository boardRepository;
+
     public void instantiateTestDatabase() throws ParseException {
         userRepository.saveAll(Arrays.asList(
-                new User("Jorge", "jorgesilva@gmail.com"),
                 new User("Caio", "caiosilveiranunes@piririm.com")
         ));
+
+        User user1 = new User("Jorge", "jorgesilva@gmail.com");
+
+        Board board1 = new Board("Board 1", user1);
+        user1.getMyBoards().add(board1);
+//        user1.getBoards().add(board1);
+//
+//        board1.getUsers().add(user1);
+
+        userRepository.save(user1);
+        boardRepository.save(board1);
     }
 }
