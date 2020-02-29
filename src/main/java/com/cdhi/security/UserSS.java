ac67313c4f4d2c1a7cf33255ce2de27745f079ef
Davi <davimargelo@hotmail.com>
2020-02-29 00:19:49 -0300

    Full USER Authentication + TOKEN Auth

---
+++ src/main/java/com/cdhi/security/UserSS.java
@@ -0,0 +1,67 @@
+package com.cdhi.security;
+
+import com.cdhi.domain.enums.Profile;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class UserSS implements UserDetails {
+
+    private Integer id;
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public UserSS() {
+    }
+
+    public UserSS(Integer id, String email, String password, Set<Profile> profiles) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.authorities = profiles.stream().map(p -> new SimpleGrantedAuthority(p.getDescription())).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/cdhi/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.cdhi.services;
+
+import com.cdhi.domain.User;
+import com.cdhi.repositories.UserRepository;
+import com.cdhi.security.UserSS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = repo.findByEmail(email);
+        if (user == null)
+            throw new UsernameNotFoundException(email);
+        return new UserSS(user.getId(), user.getEmail(), user.getPassword(), user.getProfiles());
+    }
+}
