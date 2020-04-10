51c569ceee62e17981789e940e649d2a6a329d7e
Davi <davimargelo@hotmail.com>
2020-04-10 20:38:26 -0300

    users disanled do not appear in res

--- src/main/java/com/cdhi/repositories/UserRepository.java
+++ src/main/java/com/cdhi/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,4 +30,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Transactional(readOnly = true)
     List<User> findByIdIn(List<Integer> ids);
+
+    @Transactional(readOnly = true)
+    @Query(value = "SELECT * FROM USER_ENTITY ue where LOWER(ue._NAME) like '%:nm%' and ue.ENABLED = TRUE",
+            countQuery = "SELECT COUNT(*) FROM USER_ENTITY ue where LOWER(ue._NAME) like '%:nm%' and ue.ENABLED = TRUE",
+            nativeQuery = true)
+    Page<User> findAllPageable(@Param("nm") String name, Pageable pageable);
 }
